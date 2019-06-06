c28a2f8f8db35f18d10f103f760bb27c176c77cc
EvanBC1 <starfoxz13@gmail.com>
2019-06-06 10:32:26 -0700

    adding formatting

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ function startApp() {
   // attachListeners();
 
   loadData();
+  
 }
 
 function loadData() {
@@ -21,15 +22,29 @@ function loadData() {
 }
 
 function displayHorns(animals) {
-  console.log('animals', animals);
-  animals.forEach(animal => {
+  animals.forEach(animals => {
     const $newAnimal = $('#photo-template').clone();
     
-    $newAnimal.find('img').attr('src', animal.image_url); 
-    $newAnimal.find('h2').text(animal.title); 
+    $newAnimal.find('img').attr('src', animals.image_url); 
+    $newAnimal.find('h2').text(animals.title); 
+    $newAnimal.find('p').text(animals.description);
     
     $('main').append($newAnimal);
   });
+  makedropDown(animals);
+}
+
+function makedropDown(animals) {
+  //create an array to hold keywords
+  console.log(animals);
+  const keywords = [];
+  animals.forEach((element , i) => { 
+    if(!keywords.includes(element.keyword)){
+      keywords.push(element.keyword);
+    }    
+    keywords.push(animals.keyword)
+  });
+  console.log('keyword arr', keywords)
 }
 
 $(startApp);
