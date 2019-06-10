0e3eb30fc8da2789b6e96d448f69836ddb5cdd97
EvanBC1 <starfoxz13@gmail.com>
2019-06-10 09:27:30 -0700

    added handlebars

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ var pageNum = 1;
 // var keywordsArray = [];
 
 function startApp() {
-  
-  loadCurrentPage(pageNum);
+
+  loadData(pageNum);
 
   attachListeners();
 
@@ -14,34 +14,54 @@ function startApp() {
 
 function loadData(pageNum) {
 
+
   // const success = animals => console.log('animals', animals);
   const success = animals => {
-    displayHorns(animals);
+    displayHornsHandlebars(animals);
     makedropDown(animals);
   }
 
 
   const failure = error => console.error(error);
-  
+
   $.get(`data/page-${pageNum}.json`, 'json')
     .then(success)
     .catch(failure);
 }
 
-function displayHorns(animals) {
+function displayHornsHandlebars (animals) {
   
-  animals.forEach(animals => {
-    const $newAnimal = $('.photo-template').clone();
-    
-    $newAnimal.find('img').attr('src', animals.image_url);
-    $newAnimal.find('h2').text(animals.title);
-    $newAnimal.find('p').text(animals.description);
-    $newAnimal.attr('class', animals.keyword);
-    $newAnimal.addClass('animalCard');
-    
-    $('main').append($newAnimal);
+  const template = $('#horns-template').html();
+  
+  const render = Handlebars.compile(template);
+
+  var animalElements = [];
+
+  animals.forEach(animalInfo => {
+    animalElements.push(render(animalInfo));
+    console.log();
   });
+
+  $('main').append(animalElements);
+
 }
+
+
+// function displayHorns(animals) {
+
+//   animals.forEach(animals => {
+
+//     // const $newAnimal = $('.photo-template').clone();
+
+//     // $newAnimal.find('img').attr('src', animals.image_url);
+//     // $newAnimal.find('h2').text(animals.title);
+//     // $newAnimal.find('p').text(animals.description);
+//     // $newAnimal.attr('class', animals.keyword);
+//     // $newAnimal.addClass('animalCard');
+
+//     // $('main').append($newAnimal);
+//   });
+// }
 
 function makedropDown(animals) {
   //create an array to hold keywords
@@ -59,26 +79,22 @@ function makedropDown(animals) {
 }
 
 function attachListeners(keywordsArray) {
-let type = 'photo-template';
+  let type = 'photo-template';
   $('select').on('change', event => {
     const $choice = $(event.target);
     type = $choice.val();
-    $('section').hide();
+    $('.photo-templatee').hide();
     $(`.${type}`).show();
   });
 
-    //pages listener
-    $('li').on('click', event =>{
-      pageNum = $(event.target).attr('data-page');
-      console.log(pageNum);
-      
-      $('.animalCard').remove();
-            
-      loadData(pageNum);
-     
-     
-       
-});
-}   
+  //pages listener
+  $('li').on('click', event =>{
+    pageNum = $(event.target).attr('data-page');
+    console.log(pageNum);
+    $('.photo-templatee').hide();
+   
+    loadData(pageNum);
+  });
+}
 
 $(startApp);
